d63ef861a9ad050939109e1bfbddc5cdf1c20d50
Pierre Marboutin <piballz@gmail.com>
2024-03-12 14:53:17 +0100

    added cards in index

--- app/views.py
+++ app/views.py
@@ -25,10 +25,13 @@ def date(request):
     paginator = Paginator(dates_list, 10)
 
     page = request.GET.get('page')
-    # try:
-    dates = paginator.page(page)
-    # except EmptyPage:
-    #     dates = paginator.page(paginator.num_pages)
+
+    try:
+        dates = paginator.page(page)
+    except PageNotAnInteger:
+        dates = paginator.page(1)
+    except EmptyPage:
+        dates = paginator.page(paginator.num_pages)
 
     return render(request, 'date.html', {'dates': dates})
 
@@ -40,14 +43,14 @@ def find_next_date(request):
             try:
                 current_date = datetime.strptime(current_date, '%Y-%m-%d').date()
             except ValueError:
-                error_message = "La date saisie n'est pas valide."
+                error_message = "La date saisie n'est pas valide"
                 return render(request, 'next_date.html', {'message': error_message})
 
             next_date = D_Date.objects.filter(pk_date__gt=current_date).order_by('pk_date').first()
 
             if next_date:
-                geographies_starting_with_63 = D_Geographie.objects.filter(pk_geographie__startswith='63').values_list('pk_geographie', flat=True)
-                doses_for_next_date = F_Dose.objects.filter(fk_date=next_date, fk_geographie__in=geographies_starting_with_63)
+                geographies_63 = D_Geographie.objects.filter(pk_geographie__startswith='63').values_list('pk_geographie', flat=True)
+                doses_for_next_date = F_Dose.objects.filter(fk_date=next_date, fk_geographie__in=geographies_63)
                 total_doses_for_next_date = doses_for_next_date.aggregate(total_doses=Sum('nb_doses'))['total_doses'] or 0
 
                 return render(request, 'next_date.html', {'next_date': next_date,
